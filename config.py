73512cc53723e646b783a07263bd4fea5f2da451
Yaakov Gesher <yaakov@gesher.net>
2017-06-12 13:20:53 +0300

    fixed config.py

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 # Please, copy this file into config.py and fill in the options below.
+import os
 
-TELEGRAM_KEY = "${TELEGRAM_BOT_TOKEN}"
-TRELLO_KEY = "${TRELLO_KEY}"
+TELEGRAM_KEY = os.environ.get('TELEGRAM_BOT_TOKEN', '')
+TRELLO_KEY = os.environ.get('TRELLO_KEY', '')
 
 DB_FILE = 'bot.sqlite'
 LOG_FILE = 'bot.log'
 # DEBUG, INFO, WARNING, ERROR or CRITICAL
 LOG_LEVEL = 'WARNING'
 
-TRELLO_WH_HOST = '${WEBHOOK_HOST_URL}'
+TRELLO_WH_HOST = os.environ.get('WEBHOOK_HOST_URL', '')
 TRELLO_WH_PORT = 9099
 
 # In seconds
